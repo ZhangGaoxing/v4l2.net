0dee81313e22646fd8427c392917db1d55255e02
张悦欣 <zhangyuexin121@live.cn>
2019-08-15 20:32:12 +0800

    remove xml comment

--- src/V4l2.Samples/Program.cs
+++ src/V4l2.Samples/Program.cs
@@ -3,6 +3,8 @@
 // See the LICENSE file in the project root for more information.
 
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Threading.Tasks;
